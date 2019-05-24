39a4eaeaaf19afef1fe62bf98b3064b1c04eb5b7
Oleh <olkozhushko@gmail.com>
2019-05-24 23:53:16 +0300

    fixed bugs

--- src/utils/filterText.js
+++ src/utils/filterText.js
@@ -7,13 +7,7 @@ const filterText = (data) => {
   //filter rext stripping out of all dublicated values and alphabetical characters;
   res = res.replace(/([-+/%*.]){2,}/ig, "$1").replace(/\s{2,}/g, " ").replace(/[^-+/%*\s\d.]/g, "");
   
-  if(res.includes(".")) {
-    let sliced = res.slice(res.indexOf(".") + 1);
-
-    sliced = sliced.replace(/\./g, "");
-
-    res = res.slice(0, res.indexOf(".") + 1) + sliced;
-  }
+  res = res.replace(/(\.\d+)\./g, "$1");
 
   return res;
 }
